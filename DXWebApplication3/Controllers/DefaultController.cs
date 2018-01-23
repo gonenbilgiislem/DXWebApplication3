79b2824392586ad916dca66b5e3e33f46dddc0d1
Barış Özen <bilgiislem@balikesirgonen.bel.tr>
2018-01-23 04:18:56 +0300

    Proje dosyası ekle.

---
+++ DXWebApplication3/Controllers/DefaultController.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Data;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebApplication3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        DXWebApplication3.Models.EtiketAppEntities db = new DXWebApplication3.Models.EtiketAppEntities();
+
+        [ValidateInput(false)]
+        public ActionResult GridView1Partial()
+        {
+            var model = db.Kisiler.Include(ks=>ks.Gruplar).ToList();
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialAddNew(DXWebApplication3.Models.Kisiler item)
+        {
+            var model = db.Kisiler;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Add(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialUpdate(DXWebApplication3.Models.Kisiler item)
+        {
+            var model = db.Kisiler.Include(ks=>ks.Gruplar).ToList();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var modelItem = model.FirstOrDefault(it => it.Id == item.Id);
+                    if (modelItem != null)
+                    {
+                        this.UpdateModel(modelItem);
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialDelete(System.Int32 Id)
+        {
+            var model = db.Kisiler;
+            if (Id >= 0)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.Id == Id);
+                    if (item != null)
+                        model.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+    }
+}
